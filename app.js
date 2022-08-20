065bd49298c45e43109476ff1b30fea31ba52b92
NicholasGomis <nicholasgomis@icloud.com>
2022-08-20 15:46:40 +0100

    Added rep and api links and selected html elements and stored in a variable

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+let quote = document.querySelector(".quotes")
+let btn = document.querySelector("#new-quote")
+
 
 async function getQuote(){
-
-    let response = await fetch("https://api.kanye.rest")
-    let data = await response.json();
-    // console.log(data.quote)
+   
+    let response = await fetch("https://api.kanye.rest") 
+    let data = await response.json()
+    console.log(data.quote)
 }
-getQuote();
+
+getQuote()
